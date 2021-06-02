542ec6cc2714547f0c32c1b51888c4466ab1fa58
Dianajarenga <79199013+Dianajarenga@users.noreply.github.com>
2021-06-02 08:25:21 +0300

    Add files via upload

---
+++ countdown project/revision/rev.js
@@ -0,0 +1,19 @@
+var name="Jarenga";
+console.log(name);
+let age=20;//replaces var
+console.log(age);
+const job=" Software Developer";// const cannot be reassigned
+console.log(job);
+console.log(typeof(null));//used to represent the intentional absence of an object
+let a=null;
+let b="vee"
+let c=2
+console.log(a===null);//check if object is null compare to strict operator
+console.log(Number.isNaN(a));//replaced confusion from window.isNaN
+console.log(Number.isNaN(b));
+console.log(Number.isNaN(c));
+console.log(Number.isNaN(b));//true
+console.log(isNaN(a));
+///GET VAL TO EXPLAIN IS NaN.
+
+
